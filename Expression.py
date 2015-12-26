b4b078a47d63fcd03ac7314e834d20f63916bb5d
Juliang0705 <juliang0705@gmail.com>
2015-12-25 18:30:47 -0600

    Construct the abstract syntax tree for Math Expression

---
+++ Expression.py
@@ -0,0 +1,20 @@
+"""
+The abstract syntax tree for functions
+
+type Expression = Constant
+                  Variable
+                  Sin Expression
+                  Cos Expression
+                  Tan Expression
+                  Cot Expression
+                  Sec Expression
+                  Csc Expression
+                  E   Expression
+                  Ln  Expression
+                  Plus     Expression Expression
+                  Minus    Expression Expression
+                  Multiply Expression Expression
+                  Divide   Expression Expression
+                  Power    Expression Expression
+                  Log      Expression Expression
+"""
